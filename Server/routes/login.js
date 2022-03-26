405321b0e19797c15cf6b2bcc29b4f7e329c8f4c
Richard Obaze <richardobaze@yahoo.co.uk>
2022-03-26 03:36:29 +0100

    Added backend validation of register route and login route. Added

--- Server/routes/login.js
+++ Server/routes/login.js
@@ -1,21 +1,52 @@
+require('dotenv').config();
 const Router = require('express-promise-router');
 
 const db = require('../database/database');
 
 const router =  new Router();
+const webToken = require('jsonwebtoken');
 
+// Login user route
 router.post('/', async(req, res) => {
-    const { body } = req;
-    // Need to add session creations
+    const { email, password } = req.body;
     try {
-        // Check the datebase for the users email and password if it exits
-        const { rows } = await db.query(`SELECT * FROM users WHERE email = $1 AND
-                                password = crypt($2, password)`,[body.email, body.password]);
-        if (rows.length === 0) { // If it doesnt exist return error message
-            res.json({message: "Email or password is invalid"});
-        } else { // If it exists return that accounts information
-            res.send(rows[0]);
+
+        // Check if the username and passwords fields are empty
+        if(email == undefined || email == "" || password == undefined || password == ""){
+
+            res.status(401).json({
+                message: "Fill all Fields",
+                status: res.statusCode
+
+            })
+
+        } else {
+            const { rows } = await db.query(`SELECT * FROM users WHERE email = $1 AND
+                                            password = crypt($2, password)`,[email, password]);
+            if (rows.length < 1) { // If the rows is 0 then the user email doesnt exist
+                res.status(401).json({
+                    message: "Email or Password is incorrect"
+                })
+
+            } else {
+                console.log(rows)
+                const userDetails = {
+                    firstname : rows[0].firstname,
+                    lastname : rows[0].lastname,
+                    email : rows[0].email,
+                    id: rows[0].user_id
+                }
+
+                const token = webToken.sign(userDetails, process.env.SECRET_KEY, {expiresIn: "60s"});
+
+                res.status(200).json({
+                    message: "Logged In Successfully",
+                    status: res.statusCode,
+                    token
+                })
+            }
         }
+
     } catch (err) { // If error console log the error
         console.log(err);
     }
--- Server/routes/register.js
+++ Server/routes/register.js
@@ -4,16 +4,38 @@ const db = require('../database/database');
 
 const router =  new Router();
 
+// Register User route
 router.post('/', async(req, res) => {
-    const { body } = req;
+    const { firstname, lastname, email, password, address, city, country, currency } = req.body;
     try {
-        const result = await db.query(`INSERT INTO users (firstname, lastname, email, password, address, city, country,
-                                currency) VALUES ($1, $2, $3, crypt($4, gen_salt('bf', 8)), $5, $6, $7, $8)`,[body.firstname, 
-                                body.lastname, body.email, body.password, body.address, body.city, 
-                                body.country, body.currency]);
-        res.json({message: "Registration successful"});
+        // Check if the fields are empty
+        if(firstname == undefined || firstname == "" || lastname == undefined || lastname == ""
+        || email == undefined || email == "" || password == undefined || password == ""
+        || address == undefined || address == "" || city == undefined || city == ""
+        || country == undefined || country == "" || currency == undefined || currency == ""){
+
+            // If its empty return a message fill all Fields
+            res.status(401).json({
+                message: "Fill all Fields",
+                status: res.statusCode
+
+            })
+        } else {
+            const { rows } = await db.query(`SELECT * FROM users WHERE email = $1`,[email]);
+            if (rows.length > 0) { // If the rows is greater than 0 then the email already exists
+                res.json({message: "Email is already taken"});
+            } else {
+                const result = await db.query(`INSERT INTO users (firstname, lastname, email, password, address, city, country,
+                    currency) VALUES ($1, $2, $3, crypt($4, gen_salt('bf', 8)), $5, $6, $7, $8)`,[firstname, 
+                    lastname, email, password, address, city, country, currency]);
+
+                    res.json({message: "Registration successful"});
+            }
+        }
+
+        
     } catch (err) {
-        console.log(err);
+        res.status(404).json({message : "Something went wrong"});
     }
     
 })
